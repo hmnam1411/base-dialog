bd4a9620a9f1feab5b321ca0627e837a7e0b85a4
NAM HOANG MINH <you@example.com>
2023-05-29 11:21:32 +0700

    'commit'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BaseDialog :active.sync="show">
+      <h1>Test</h1>
+      <button type="button" @click="show = !show">Close</button>
+    </BaseDialog>
+    <button type="button" @click="show = !show">Toggle</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseDialog from "./components/BaseDialog.vue";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    BaseDialog
+  },
+  data() {
+    return {
+      show: false
+    };
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
